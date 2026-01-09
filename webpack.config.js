85ef41666c5c63b00f14b9de9b72b6e854ad7712
Ronald Huereca <ronald@mediaron.com>
2026-01-08 23:13:06 -0600

    Adding sourcemaps.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
 
-module.exports = {
-	...defaultConfig,
-	entry: {
-		'cmds-block-editor': './src/commands/commands-block-editor.js',
-		'cmds-admin': './src/commands/commands-admin.js',
-		'cmds-frontend': './src/commands/commands-frontend.js',
-		'cmds-network': './src/commands/commands-network.js',
-	},
-	output: {
-		path: path.resolve(process.cwd(), 'build'),
-		filename: '[name].js',
-	},
+module.exports = ( env ) => {
+	return [
+		{
+			...defaultConfig,
+			mode: env.mode,
+			devtool: env.mode === 'development' ? 'source-map' : false,
+			entry: {
+				'cmds-block-editor': './src/commands/commands-block-editor.js',
+				'cmds-admin': './src/commands/commands-admin.js',
+				'cmds-frontend': './src/commands/commands-frontend.js',
+				'cmds-network': './src/commands/commands-network.js',
+			},
+			output: {
+				path: path.resolve(process.cwd(), 'build'),
+				filename: '[name].js',
+			},
+		}
+	]
 };
